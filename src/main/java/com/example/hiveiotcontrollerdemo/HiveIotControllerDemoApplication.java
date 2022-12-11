99d326c6d3fdda331c8e39e189836dbceb9fb874
henrieessalu <henri.eessalu@gmail.com>
2022-12-11 20:18:00 +0200

    initial commit

---
+++ src/main/java/com/example/hiveiotcontrollerdemo/HiveIotControllerDemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.hiveiotcontrollerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class HiveIotControllerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HiveIotControllerDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hiveiotcontrollerdemo/HiveIotControllerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hiveiotcontrollerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiveIotControllerDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
